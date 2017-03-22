86cad10216737d7e5507fcedaca94d30685657f6
Joseph-Weiner <joseph.weiner1@marist.edu>
2017-03-22 15:56:36 -0400

    Update Problem10_23Test.java
    Just fixing the test program so it still works with the class program

--- labs/7/Problem10_23Test.java
+++ labs/7/Problem10_23Test.java
@@ -1,17 +1,17 @@
 package Lab7;
-import Lab7.Problem10_23.MyString;
+import Lab7.Problem10_23.MyString2;
 public class Problem10_23Test {
 
 	public static void main(String[] args) {
 		
-		System.out.println(new MyString("I'm a string.").toChars());
-        MyString s1 = new MyString("test123");
-        MyString s2 = new MyString("test123");
+		System.out.println(new MyString2("I'm a string.").toChars());
+        MyString2 s1 = new MyString2("test123");
+        MyString2 s2 = new MyString2("test123");
         System.out.println("s1 = " + s1.toString());
         System.out.println("s2 = " + s2.toString());
         System.out.println("Is s1 equal to s2? " + s1.compare(s2));
         System.out.println("Value of false changed to uppercase = " + (MyString.valueOf(false).toUpperCase().toString()));
-        System.out.println("Value of true = " + (MyString.valueOf(true).toString() ));
+        System.out.println("Value of true = " + (MyString2.valueOf(true).toString() ));
 
 
 	}
