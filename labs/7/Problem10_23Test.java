93183a5c51ac7cabdd6370bb5c1255a6de89c5be
Juan Arias <juan.arias@marist.edu>
2017-03-26 09:47:50 -0400

    Feedback on Lab 7

--- labs/7/Problem10_23Test.java
+++ labs/7/Problem10_23Test.java
@@ -10,7 +10,7 @@ public class Problem10_23Test {
         System.out.println("s1 = " + s1.toString());
         System.out.println("s2 = " + s2.toString());
         System.out.println("Is s1 equal to s2? " + s1.compare(s2));
-        System.out.println("Value of false changed to uppercase = " + (MyString.valueOf(false).toUpperCase().toString()));
+        System.out.println("Value of false changed to uppercase = " + (MyString2.valueOf(false).toUpperCase().toString()));
         System.out.println("Value of true = " + (MyString2.valueOf(true).toString() ));
 
 
